8dd7a63f3bf5cc4bed2656b0dc06dc93694bb6ce
amanda <amanda@zipcodes-MacBook-Pro-2.local>
2020-11-05 16:44:32 -0500

    update

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,10 +4,18 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
-    public Board(Character[][] matrix) {
+    Character[][] board;
+
+    public Board(Character[][] matrix) {  //an array of arrays aka tic-tac-toe board
+
+        this.board = matrix;
     }
+ // check all rows, then check all columns, then all diagonals
 
     public Boolean isInFavorOfX() {
+
+        board[0][0]
+
         return null;
     }
 
